7f28b5b999d750bf3fc2a690731cd08e3533a87d
Haerin <soundzrinjhr@gmail.com>
2022-08-29 12:00:54 +0900

    BubbleSort

---
+++ src/BubbleSort.java
@@ -0,0 +1,38 @@
+import java.io.*;
+
+public class BubbleSort {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int size = Integer.parseInt(br.readLine());
+        int[] numsArray = new int[size];
+
+        // 배열 입력받기
+        for (int i = 0; i < size; i++) {
+            numsArray[i] = Integer.parseInt(br.readLine());
+            System.out.println("numsArray[" + i + "] = " + numsArray[i]);
+        }
+
+        // 정렬 전 배열 출력
+        System.out.print("정렬 전 numsArray={ ");
+        for (int i = 0; i < size; i++) {
+            System.out.print(numsArray[i] + " ");
+        }
+        System.out.print("}\n");
+
+        // 정렬하는 코드
+        int temp = 0;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size - 1; j++) {
+                if (numsArray[j + 1] < numsArray[j]) {
+                    temp = numsArray[j];
+                    numsArray[j] = numsArray[j + 1];
+                    numsArray[j + 1] = temp;
+                }}}
+
+        // 정렬 후 배열 출력
+        System.out.print("정렬 후 numsArray={ ");
+        for (int i = 0; i < size; i++) {
+            System.out.print(numsArray[i] + " ");
+        }
+        System.out.print("}\n");
+    }}
